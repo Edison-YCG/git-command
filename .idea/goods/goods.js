9d0361e1e356a82472753fd7398df74579c0286c
NaivetyEvil <1325225487@qq.com>
2018-08-19 17:34:43 +0800

    feat(goods):新增商品模块

---
+++ .idea/goods/goods.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-08-19 17:25:36
+ * @version $Id$
+ */
+var top_h = document.getElementById('top_h')
+var text = '文字标题已被替换'
+top_h.innerHTML.text
+function getGoods(){}
